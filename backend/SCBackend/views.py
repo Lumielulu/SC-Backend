5a6e0cfcf2323bb8a84d5e1d4a04b15fc44c12fd
ubuntu-miku <plazasjolifenzio@gmail.com>
2025-09-19 02:30:36 -0300

    DSADSA

--- backend/SCBackend/views.py
+++ backend/SCBackend/views.py
@@ -9,9 +9,6 @@ from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 
 
-#este es mi backen
-
-
 
 @csrf_exempt
 @require_http_methods(['POST'])
