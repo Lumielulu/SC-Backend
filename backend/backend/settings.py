0b7020beaa64f60c77f620a64d023b557508922c
ubuntu-miku <plazasjolifenzio@gmail.com>
2025-10-08 13:18:16 -0300

    a

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(dotenv_path=(BASE_DIR.parent.parent  / '.mikufile'))
+load_dotenv(dotenv_path=(BASE_DIR.parent.parent.parent  / '.mikufile'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
