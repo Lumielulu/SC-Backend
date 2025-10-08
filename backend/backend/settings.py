0dc94ed53477820b89f49c5f029ce7cc756d5964
ubuntu-miku <plazasjolifenzio@gmail.com>
2025-10-08 13:27:08 -0300

    intentop 3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = [h.strip() for h in os.getenv("ALLOWED_HOSTS", "localhost").split(",")]
 
 
 # Application definition
