9d805aeb770ce3d04b8e24ef18198d0430dedde9
issusk0 <plazasjolifenzio@gmail.com>
2025-10-22 10:37:16 -0300

    migracion a RDS

--- backend/SCBackend/models.py
+++ backend/SCBackend/models.py
@@ -4,10 +4,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+
+class Artista(models.Model):
+    nombre_artista = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre_artista
+
 class Song(models.Model):
     audio_file = models.FileField(upload_to='songs/', default='')
     image_file = models.FileField(upload_to='image/',default='')
-    titulo = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=100)
+    duracion = models.PositiveIntegerField()
+
 
     def __str__(self):
         return self.titulo or self.song_url or self.image_url
@@ -27,6 +35,16 @@ class Song(models.Model):
         return ''
 
 
+class ArtistaAlbum (models.Model):
+    url_album = models.URLField(max_length=2000)
+    fecha_lanzamiento = models.DateField(auto_now_add=True)
+    artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
+
+class AlbumCancion(models.Model):
+    id_cancion = models.ForeignKey(Song, on_delete=models.CASCADE)
+    id_album   = models.ForeignKey(ArtistaAlbum, on_delete=models.CASCADE)
+
+#inicio de relaciones playlist-usuario
 class CustomUser(AbstractUser):
     image_file = models.FileField(upload_to='profile_pictures/', blank=True, null=True)
 
@@ -39,3 +57,32 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.profile_image_url
+
+class Playlist(models.Model):
+    nombre_playlist = models.CharField(max_length=100, null=True, blank=True)
+
+
+class PlaylistCanciones(models.Model):
+    id_cancion = models.ForeignKey(Song, on_delete=models.CASCADE)
+    id_playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+
+
+class Reaccion(models.Model):
+    LIKE_CHOICES = [
+        ("like","Like"),
+        ("dislike", "Dislike")
+    ]
+    like = models.CharField(max_length=10, choices=LIKE_CHOICES)
+    timestamp = models.DateField(auto_now_add=True)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    cancion_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+
+class Reproduccion(models.Model):
+    fecha_reproduccion = models.DateField(auto_now_add=True)
+    cancion_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    artista_id = models.ForeignKey(Artista, on_delete=models.CASCADE)
+
+
+class HistorialReproducciones(models.Model):
+    id_reproduccion = models.ForeignKey(Reproduccion, on_delete=models.CASCADE)
