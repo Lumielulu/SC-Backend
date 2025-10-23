6fbdd8dfe7f2d0aa3fcedf656afdb90af31d741d
ubuntu-miku <plazasjolifenzio@gmail.com>
2025-10-22 21:09:46 -0300

    Agregado de admin sites y ajuste de static files para pruebas con collect static

--- backend/SCBackend/admin.py
+++ backend/SCBackend/admin.py
@@ -3,3 +3,11 @@ from .models import *
 # Register your models here.
 admin.site.register(Song)
 admin.site.register(CustomUser)
+admin.site.register(Artista)
+admin.site.register(ArtistaAlbum)
+admin.site.register(AlbumCancion)
+admin.site.register(Playlist)
+admin.site.register(PlaylistCanciones)
+admin.site.register(Reaccion)
+admin.site.register(Reproduccion)
+admin.site.register(HistorialReproducciones)
