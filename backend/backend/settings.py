ea9850d96a82a6f65d0180699ea04a62f02b3882
ubuntu-miku <plazasjolifenzio@gmail.com>
2025-11-12 19:14:45 -0300

    CONEXION

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,23 +118,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-    #*
-    # 'ENGINE': 'django.db.backends.postgresql',
-    #    'NAME': os.getenv("DB_NAME"),
-    #    'USER': os.getenv("DB_USER"),
-     #   'PASSWORD': os.getenv("DB_PASSWORD"),
-     #   'HOST': os.getenv("DB_HOST"),
-     #   'PORT': os.getenv("DB_PORT"),
-    #}
 
 
 DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600
-        )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+    }
 }
-
 
 
 #*
