9f7a13ce666429320cd4ce8e8a95274b54a663f3
ubuntu-miku <plazasjolifenzio@gmail.com>
2025-11-12 20:08:39 -0300

    ascesea

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -202,7 +202,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'us-east-1'  # e.g., 'us-east-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'  # Recommended for security
