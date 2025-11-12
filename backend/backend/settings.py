c4504f42b0e473212b929d8c12eac9119d0c179e
ubuntu-miku <plazasjolifenzio@gmail.com>
2025-11-12 20:43:59 -0300

    DOMAIN

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -206,7 +206,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'us-east-1'  # e.g., 'us-east-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'  # Recommended for security
 AWS_S3_FILE_OVERWRITE = False  # Set to True if you want to overwrite files with the same name
-AWS_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
