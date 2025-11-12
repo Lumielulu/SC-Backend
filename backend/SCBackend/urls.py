2aaebfa1f67676204e9f4b0a964e753d7cd52419
ubuntu-miku <plazasjolifenzio@gmail.com>
2025-11-12 18:45:04 -0300

    INTNETNO3

--- backend/SCBackend/urls.py
+++ backend/SCBackend/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name = 'token_obtain_pair'),
-    path('login/', views.test_angular, name='test_angular'),
+    #path('login/', views.test_angular, name='test_angular'),
     path('streaming/<int:song_id>/', views.stream_master, name="streaming_master"),
     path('songs/', views.get_songs, name='get_songs'),
     path('users/', views.getUsers, name='users'),
